8f3fa20bf0b4a813e698c7a0bd9524f7e2a56f34
imopbuilder <opbuilder9@gmail.com>
2024-02-26 17:48:27 +0530

    feat: add SEO to registration page

--- src/app/users/registration/page.tsx
+++ src/app/users/registration/page.tsx
@@ -1,7 +1,12 @@
 import { Footer } from '@/components/global/footer';
 import { Header } from '@/components/global/header';
 import { Registrations } from '@/components/pages/users/registration';
+import { Metadata } from 'next';
 import { Fragment } from 'react';
+
+export const metadata: Metadata = {
+  title: 'Registration',
+};
 
 export default function Page() {
   return (
