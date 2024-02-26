9619a59db2cea56b250f2db78bea145d0cc7ee82
imopbuilder <opbuilder9@gmail.com>
2024-02-26 15:52:20 +0530

    enhance: add and link logo to app

--- src/components/global/header/index.tsx
+++ src/components/global/header/index.tsx
@@ -9,15 +9,7 @@ export function Header() {
         <div className='w-full'>
           <Link href={'/'} className='inline-flex items-center justify-start font-medium gap-2.5'>
             <span className='size-8 inline-flex items-center justify-center bg-background rounded-full'>
-              <Image
-                src='https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Travel%20and%20places/Fire.png'
-                className=''
-                width='25'
-                height='25'
-                alt='Logo'
-                loading='lazy'
-                unoptimized
-              />
+              <Image src='/images/ui/app/logo.png' className='' width='25' height='25' alt='Logo' loading='lazy' unoptimized />
             </span>
             <span className='text-sm font-medium inline-block relative top-0.5'>Eblaze</span>
           </Link>
