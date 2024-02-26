d1335084d596b4f2e39ca9d7fd7e7f67fc1db71c
imopbuilder <opbuilder9@gmail.com>
2024-02-26 13:35:42 +0530

    enhance: add logo to header section

--- src/components/global/header/index.tsx
+++ src/components/global/header/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import { AppProgress, NavHelperBackgrounds, NavMenuBtn } from './client';
 
@@ -6,13 +7,19 @@ export function Header() {
     <header className='bg-primary-color text-background sticky top-0 w-full z-20'>
       <div className='px-5 h-14 flex items-center justify-between relative'>
         <div className='w-full'>
-          <Link href={'/'} className='bg-background inline-flex size-8 items-center justify-center rounded-full font-medium'>
-            <img
-              src='https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Travel%20and%20places/Fire.png'
-              alt='Fire'
-              width='25'
-              height='25'
-            />
+          <Link href={'/'} className='inline-flex items-center justify-start font-medium gap-2.5'>
+            <span className='size-8 inline-flex items-center justify-center bg-background rounded-full'>
+              <Image
+                src='https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Travel%20and%20places/Fire.png'
+                className=''
+                width='25'
+                height='25'
+                alt='Logo'
+                loading='lazy'
+                unoptimized
+              />
+            </span>
+            <span className='text-sm font-medium inline-block relative top-0.5'>Eblaze</span>
           </Link>
         </div>
         <div className='flex items-center justify-center gap-8'>
