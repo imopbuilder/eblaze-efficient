7a1c3866ba50c8d12b5be7b7089dedb65e2fe92d
imopbuilder <opbuilder9@gmail.com>
2024-03-06 19:04:43 +0530

    docs: add eblaze about section

--- src/constants/about.ts
+++ src/constants/about.ts
@@ -3,16 +3,16 @@ export const ABOUT = [
     id: 'a1',
     title: 'Eblaze',
     description: [
-      "To illustrate this animation, I'll go step by step, animating a paragraph on scroll first, then moving to a word by word animation and then doing a character by character animation. Depending on your taste (I like the word by word the best), you can choose whichever implementation you prefer.",
-      "To illustrate this animation, I'll go step by step, animating a paragraph on scroll first, then moving to a word by word animation and then doing a character by character animation. Depending on your taste (I like the word by word the best), you can choose whichever implementation you prefer.",
+      'Eblaze, the brainchild of the Department of Electrical and Electronics Engineering at Sri Venkateswara University College of Engineering, Tirupati, goes beyond the confines of a conventional college event. It emerges as a catalyst for innovation, a hub for the exchange of knowledge, and a celebration of collaborative spirit.',
+      'At Eblaze, through its meticulously curated workshops and training sessions, Eblaze provides a platform for participants to explore the realms of electronics and electrical engineering. Join us at Eblaze to unleash your creativity, expand your horizons, and embark on a journey of transformative learning and discovery!',
     ],
   },
   {
     id: 'a2',
     title: 'Department',
     description: [
-      "To illustrate this animation, I'll go step by step, animating a paragraph on scroll first, then moving to a word by word animation and then doing a character by character animation. Depending on your taste (I like the word by word the best), you can choose whichever implementation you prefer.",
-      "To illustrate this animation, I'll go step by step, animating a paragraph on scroll first, then moving to a word by word animation and then doing a character by character animation. Depending on your taste (I like the word by word the best), you can choose whichever implementation you prefer.",
+      'The Department of Electrical and Electronics Engineering at Sri Venkateswara University College of Engineering, Tirupati has been a cornerstone of academic excellence and innovation since its inception in 1959. Over the years, it has continuously evolved to meet the educational and research needs of students, industry, and society at large.',
+      " The department offers a comprehensive range of programs, including a Bachelor's in Technology (B. Tech) in Electrical and Electronics Engineering and a Master's program specializing in Power Systems. Both the undergraduate and postgraduate programs have earned accreditation from the National Board of Accreditation (NBA) in New Delhi, attesting to their high-quality education and research standards.",
     ],
   },
 ];
