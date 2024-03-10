4c71e413687958b838dd336a61447e8b22f48f9a
imopbuilder <opbuilder9@gmail.com>
2024-03-10 21:57:04 +0530

    feat: add terms-of-service page

---
+++ src/app/faqs/page.tsx
@@ -0,0 +1,25 @@
+import { Footer } from '@/components/global/footer';
+import { Header } from '@/components/global/header';
+import { RegisterCard } from '@/components/global/register-card';
+import { HomeFaqs } from '@/components/pages/home';
+import { Metadata } from 'next';
+import { Fragment } from 'react';
+
+export const metadata: Metadata = {
+  title: 'Faqs',
+  description:
+    "Eblaze offers top-notch workshops in electrical and electronics engineering. Join us for hands-on learning and stay ahead in today's tech world!",
+};
+
+export default function Page() {
+  return (
+    <Fragment>
+      <Header />
+      <main>
+        <RegisterCard />
+        <HomeFaqs />
+        <Footer />
+      </main>
+    </Fragment>
+  );
+}
---
+++ src/constants/terms-of-service.ts
@@ -0,0 +1,46 @@
+export const TERMS_OF_SERVICE = [
+  {
+    heading: 'Registration',
+    description: 'By registering for the Eblaze workshop, participants agree to adhere to all terms and conditions outlined herein.',
+  },
+  {
+    heading: 'Eligibility',
+    description:
+      'Participation in the workshop is open to individuals who meet the eligibility criteria specified for each event, track, or contest.',
+  },
+  {
+    heading: 'Submission Guidelines',
+    description:
+      'Participants submitting papers, projects, or presentations must ensure that their submissions comply with the specified guidelines, including originality, formatting, and submission deadlines.',
+  },
+  {
+    heading: 'Code of Conduct',
+    description:
+      'Participants are expected to maintain a respectful and professional demeanor throughout the workshop. Any form of harassment, discrimination, or misconduct will not be tolerated and may result in disqualification or removal from the event.',
+  },
+  {
+    heading: 'Intellectual Property',
+    description:
+      'All intellectual property rights related to submissions, including papers, projects, presentations, and any associated materials, remain with the respective participants. However, by submitting their work, participants grant Eblaze and its affiliates the right to use, reproduce, and distribute their submissions for promotional and educational purposes.',
+  },
+  {
+    heading: 'Prize Distribution',
+    description:
+      "Cash prizes and certificates will be awarded to the top performers in each track or contest, with the prize amounts varying depending on the specific event. Winners will be announced during the workshop, and prize distribution will be conducted in accordance with the event's guidelines.",
+  },
+  {
+    heading: 'Liability',
+    description:
+      'Eblaze and its affiliates are not liable for any loss, damage, or injury incurred by participants during the workshop, including but not limited to travel-related incidents, technical difficulties, or any other unforeseen circumstances.',
+  },
+  {
+    heading: 'Modification of Terms',
+    description:
+      'Eblaze reserves the right to modify or update these terms and conditions at any time without prior notice. Participants are responsible for regularly reviewing the terms to stay informed of any changes.',
+  },
+  {
+    heading: 'Outro',
+    description:
+      'By registering for the Eblaze workshop, participants acknowledge that they have read, understood, and agreed to abide by these terms and conditions.',
+  },
+];
