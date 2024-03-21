916a7c38c7f176e7c7fc663393e7e69efb94b69f
imopbuilder <opbuilder9@gmail.com>
2024-03-21 19:35:04 +0530

    enhance: lighthouse score

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -20,19 +20,21 @@ interface AccordionTriggerProps extends React.ComponentPropsWithoutRef<typeof Ac
 
 const AccordionTrigger = React.forwardRef<React.ElementRef<typeof AccordionPrimitive.Trigger>, AccordionTriggerProps>(
   ({ className, children, showIcon = true, ...props }, ref) => (
-    <AccordionPrimitive.Header className='flex'>
-      <AccordionPrimitive.Trigger
-        ref={ref}
-        className={cn(
-          'flex flex-1 items-center justify-between py-4 text-sm font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180',
-          className,
-        )}
-        {...props}
-      >
-        {children}
-        {showIcon ? <ChevronDownIcon className='h-4 w-4 shrink-0 text-muted-foreground transition-transform duration-200' /> : null}
-      </AccordionPrimitive.Trigger>
-    </AccordionPrimitive.Header>
+    <h1>
+      <AccordionPrimitive.Header className='flex' asChild>
+        <AccordionPrimitive.Trigger
+          ref={ref}
+          className={cn(
+            'flex flex-1 items-center justify-between py-4 text-sm font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180',
+            className,
+          )}
+          {...props}
+        >
+          {children}
+          {showIcon ? <ChevronDownIcon className='h-4 w-4 shrink-0 text-muted-foreground transition-transform duration-200' /> : null}
+        </AccordionPrimitive.Trigger>
+      </AccordionPrimitive.Header>
+    </h1>
   ),
 );
 AccordionTrigger.displayName = AccordionPrimitive.Trigger.displayName;
